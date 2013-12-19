5071a35e15dacd85542eda1894e1d001fdf5a04c
LikeMyBread <mbradley31@outlook.com>
2013-12-18 20:11:39 -0500

    Added interface for choosing which generator to use

--- GeneratorSite.py
+++ GeneratorSite.py
@@ -17,22 +17,75 @@ global random
 
 
 @app.route('/')
-def startHere():
-    mName = NameFunction.NameFunc('m')
-    fName = NameFunction.NameFunc('f')
-    organization = OrgFunction.OrgFunc()
-    town = TownFunction.TownFunc()
+def startHere(result=""):
+#    mName = NameFunction.NameFunc('m')
+#    fName = NameFunction.NameFunc('f')
+#    organization = OrgFunction.OrgFunc()
+#    town = TownFunction.TownFunc()
 
     
-    html = '''<div style="width: 500px; height: 300px; margin:auto;text-align:center">Mike's Generators</div>'''+'Male Name: '+ mName +'<div/>'+ 'Female Name: '+fName +'<div/>'+'<div/>'+ 'Organization: '+ organization +'<div/>'+'<div/>'+'Town: '+ town
+    html = '''<html>
+
+<head>
+<title>Mike's Random Generators</title>
+<style type="text/css">
+.genButton{background-color: Aquamarine; height: 30px; width: 100px;float:left;margin:0px auto;text-align:center}
+</style>
+</head>
+
+
+<body>
+
+<div id="container" style="width:100%;min-width:500px">
+
+<div id="heading" style="background-color:Aquamarine">
+	<h1 style="margin:0px auto;text-align:center">Mike's Random Generators</h1>
+</div>
+
+<div id="buttonBar" style="width:100px;float:left">
+	
+	<a href="/malename">
+		<div class="genButton">Male Name</div>
+	</a>
+	<a href="/femalename">
+		<div class="genButton">Female Name</div>
+	</a>
+	<a href="/hometown">
+		<div class="genButton">Hometown</div>
+	</a>
+	<a href="/organization">
+		<div class="genButton">Organization</div>
+	</a>
+</div>
+
+<div id="resultsArea" style="width:80%;min-width:100px;float:left">
+'''+result+'''
+</div>
+
+</div>
+
+</body>
+</html>'''
+
     return(html)
 
+#Generators go down here
 
+@app.route("/malename")
+def maleName():
+    return(startHere(NameFunction.NameFunc('m')))
 
+@app.route("/femalename")
+def femaleName():
+    return(startHere(NameFunction.NameFunc('f')))
 
+@app.route("/hometown")
+def maleName():
+    return(startHere(TownFunction.TownFunc()))
 
-
-
+@app.route("/organization")
+def organization():
+    return(startHere(TownFunction.TownFunc()))
 
 
 
